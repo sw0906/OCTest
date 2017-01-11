3f329827e887593b7515c3d35231149ca136106c
SW <shouweiwei@dbs.com>
2017-01-11 17:57:03 +0800

    update bs

--- OCTest/BinarySearchTest.m
+++ OCTest/BinarySearchTest.m
@@ -29,7 +29,9 @@
 {
 //    [self testOccur];
 //    [self testMaxInMountain];
-    [self testPeak];
+//    [self testPeak];
+//    [self testBVersion];
+    [self testSRS];
 }
 
 #pragma mark - Total Occurrence of Target
@@ -142,24 +144,27 @@
 }
 
 #pragma mark - Find Peak Element
-//There is an integer array which has the following features:
-//
-//The numbers in adjacent positions are different.
-//A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
-//We define a position P is a peek if:
-//
-//A[P] > A[P-1] && A[P] > A[P+1]
-//Find a peak element in this array. Return the index of the peak.
-//
-//Notice
-//
-//The array may contains multiple peeks, find any of them.
-//
-//Have you met this question in a real interview? Yes
-//Example
-//Given [1, 2, 1, 3, 4, 5, 7, 6]
-//
-//Return index 1 (which is number 2) or 6 (which is number 7)
+/*
+There is an integer array which has the following features:
+
+The numbers in adjacent positions are different.
+A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+We define a position P is a peek if:
+
+A[P] > A[P-1] && A[P] > A[P+1]
+Find a peak element in this array. Return the index of the peak.
+
+Notice
+
+The array may contains multiple peeks, find any of them.
+
+Have you met this question in a real interview? Yes
+Example
+Given [1, 2, 1, 3, 4, 5, 7, 6]
+
+Return index 1 (which is number 2) or 6 (which is number 7)
+ 
+*/
 
 //int findPeak(vector<int> A) {
 
@@ -198,4 +203,134 @@
         return [self findPeakWithArray:arr leftIndex:mid rightIndex:r];
     }
 }
+
+
+#pragma mark -  First Bad Version
+/*
+The code base version is an integer start from 1 to n. One day, someone committed a bad version in the code case, so it caused this version and the following versions are all failed in the unit tests. Find the first bad version.
+
+You can call isBadVersion to help you determine which version is the first bad one. The details interface can be found in the code's annotation part.
+
+Notice
+
+Please read the annotation in code area to get the correct way to call isBadVersion in different language. For example, Java is SVNRepo.isBadVersion(v)
+
+Have you met this question in a real interview? Yes
+Example
+Given n = 5:
+
+isBadVersion(3) -> false
+isBadVersion(5) -> true
+isBadVersion(4) -> true
+Here we are 100% sure that the 4th version is the first bad version.
+*/
+
+//int findFirstBadVersion(int n) {
+
+-(void)testBVersion
+{
+    NSInteger version = [self findFirstBadVersion:22];
+}
+
+
+- (BOOL)isBadVersion:(NSInteger)n
+{
+    NSInteger badVersion = 7;
+    return n>=badVersion ? YES : NO ;
+}
+
+
+
+
+
+//main
+- (NSInteger)findFirstBadVersion:(NSInteger)n
+{
+    return [self findFBV_left:0 right:n];
+}
+
+- (NSInteger)findFBV_left:(NSInteger)l right:(NSInteger)r
+{
+    if ( l == r-1 ) {
+        return [self isBadVersion:l] ? l : r;
+    }
+    
+    NSInteger m = l + (r-l)/2;
+    if ([self isBadVersion:m]) {
+        return [self findFBV_left:l right:m];
+    }
+    else
+    {
+        return [self findFBV_left:m right:r];
+    }
+}
+
+#pragma mark - Search in Rotated Sorted Array
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Have you met this question in a real interview? Yes
+Example
+For [4, 5, 1, 2, 3] and target=1, return 2.
+
+For [4, 5, 1, 2, 3] and target=0, return -1.
+*/
+
+//int search(vector<int> &A, int target) {
+
+
+
+-(void)testSRS
+{
+    NSArray *arr = @[@(4), @(5), @(1), @(2), @(3)];
+    NSInteger re = [self searchRSarray:arr target:@(6)];
+    re = [self searchRSarray:arr target:@(2)];
+    re = [self searchRSarray:arr target:@(1)];
+}
+
+//main
+- (NSInteger)searchRSarray:(NSArray *)a target:(NSNumber *)target
+{
+    return [self searchRSAleft:0 right:a.count-1 target:target array:a];
+}
+
+- (NSInteger)searchRSAleft:(NSInteger)l right:(NSInteger)r target:(NSNumber *)t array:(NSArray *)a
+{
+    if (l == r-1) {
+        if (a[l] == t)
+            return l;
+        else if(a[r] == t)
+            return r;
+        else
+            return -1;
+    }
+    
+    NSInteger m = l + (r-l)/2;
+    if (a[l] < a[m]) {
+        if (t >= a[l] && t <= a[m]) {
+            return [self searchRSAleft:l right:m target:t array:a];
+        }
+        else
+        {
+            return [self searchRSAleft:m right:r target:t array:a];
+        }
+    }
+    else
+    {
+        if (t >= a[m] && t <= a[r]) {
+            return [self searchRSAleft:m right:r target:t array:a];
+        }
+        else
+        {
+            return [self searchRSAleft:l right:m target:t array:a];
+        }
+    }
+}
+
 @end
